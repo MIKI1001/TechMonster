02dd6fbcc291d6cf2fd7a4a6520e9af73c74a10f
篠田弥樹 <2002ikim@gmail.com>
2015-11-01 16:54:58 +0900

    first commit

--- TechMonster/LobbyViewController.swift
+++ TechMonster/LobbyViewController.swift
@@ -7,13 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
-class LobbyViewController: UIViewController {
+class LobbyViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var stamina: Float = 0
+    var staminaTimer: NSTimer!
+    var util: TechDraUtility!
+    var player: Player!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var staminaBar: UIProgressView!
+    @IBOutlet var levelLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        player = Player()
+        
+        staminaBar.transform = CGAffineTransformMakeScale(1.0, 4.0)
+        
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        var level: Int = userDefaults.integerForKey("level")
+        
+        nameLabel.text = player.name
+        levelLabel.text = String(format: "Lv. %d", level + 1)
+        stamina = 100
+        
+        util = TechDraUtility()
+        cureStamina()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        util.playBGM("Lobby")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        util.stopBGM()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +52,16 @@ class LobbyViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func cureStamina() {
+        staminaTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "updateStaminaValue", userInfo: nil,repeats: true)
+        staminaTimer.fire()
+    }
+    
+    func updateStaminaValue() {
+        if stamina <= 100 {
+            staminaBar.progress = stamina / 100
+        }
+    }
 
     /*
     // MARK: - Navigation
--- TechMonster/ViewController.swift
+++ TechMonster/ViewController.swift
@@ -9,16 +9,88 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer: NSTimer!
+    var enemyTimer: NSTimer!
+    
+    var enemy: Enemy!
+    var player: Player!
+    
+    let util: TechDraUtility = TechDraUtility()
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var attackButton: UIButton!
+    
+    @IBOutlet var enemyImageView: UIImageView!
+    @IBOutlet var playerImageView: UIImageView!
+    
+    @IBOutlet var enemyHPBar: UIProgressView!
+    @IBOutlet var playerHPBar: UIProgressView!
+    
+    @IBOutlet var enemyNameLabel: UILabel!
+    @IBOutlet var playerNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        enemyHPBar.transform = CGAffineTransformMakeScale(1.0, 4.0)
+        playerHPBar.transform =CGAffineTransformMakeScale(1.0, 4.0)
+        initStatus()
+        
+        enemyTimer = NSTimer.scheduledTimerWithTimeInterval(NSTimeInterval(enemy.speed), target: self, selector: "enemyAttackk", userInfo: nil, repeats: true)
+        enemyTimer.fire()
+    }
+    
+    func initStatus() {
+        enemy = Enemy()
+        player = Player()
+        
+        enemyNameLabel.text = enemy.name
+        playerNameLabel.text = player.name
+        
+        enemyImageView.image = player.image
+        playerImageView.image = player.image
+        
+        enemyHPBar.progress = enemy.currentHP / enemy.maxHP
+        playerHPBar.progress = player.currentHP / player.maxHP
+        
+        cureHP()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        util.playBGM("BGM_battle001")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func playerAttack() {
+        TechDraUtility.damageAnimation(playerImageView)
+        util.playSE("SE_attack")
+        
+        enemy.currentHP = enemy.currentHP - player.attackPoint
+        enemyHPBar.setProgress(enemy.curretHP / enemy.maxHP, animated: true)
+        
+        if enemy.currentHP <= 0.0 {
+            finishBattle(enemyImageView, winPlayer: true)
+        }
+    }
+    func enemyAttack() {
+        TechDraUtility.damageAnimation(playerImageView)
+        util.playSE("SE_attack")
+        
+        player.currentHP = player.currentHP - player.attackPoint
+        playerHPBar.setProgress(player.curretHP / enemy.maxHP, animated: false)
+        
+        if enemy.currentHP <= 0.0 {
+            finishBattle(playerImageView, winPlayer: false)
+        }
+    }
+    
+    func finishBattle(vanishImageView
+    
 
 
 }
